2b8ee2a073f5bd4ec3745c3dc8f273b0bdd497c3
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-03-20 16:44:10 +0100

    Add html page, add functions to all main functions

--- script.js
+++ script.js
@@ -26,16 +26,16 @@ const gameboard = (function () {
         return (board[row][column] === "");
     }
 
-    function markChosenCell(row, column, player) {
+    function markChosenCell(row, column) {
         // CHANGE TO SELECTED PLAYER SYMBOL
         board[row][column] = "o";
     }
 
-    return { chosenCellEmpty, createBoard, printBoard };
+    return { chosenCellEmpty, createBoard, printBoard, markChosenCell };
 })();
 
-gameboard.createBoard();
-gameboard.printBoard();
+// gameboard.createBoard();
+// gameboard.printBoard();
 
 
 
@@ -44,22 +44,32 @@ gameboard.printBoard();
 // what marker does each player have?
 const players = (function () {
     const playersList = [];
-    // ask for player's name
+
     function askForPlayerName() {
-        const name = prompt("Type in your name:");
-    }
-    function createPlayer(name, symbol) {
-        return { name, symbol, score: 0 };
+        const name1 = prompt("Player 1 name:");
+        const symbol1 = "X";
+        const name2 = prompt("Player 2 name:");
+        const symbol2 = "O";
+
+        createPlayer(name1, symbol1);
+        createPlayer(name2, symbol2);
     }
 
-    return { createPlayer };
+    function createPlayer(name, symbol) {
+        playersList.push({ name, symbol, score: 0 });
+        // console.log(playersList);
+    }
+
+    function getPlayersList() {
+        return playersList;
+    }
+
+    return { askForPlayerName, createPlayer, getPlayersList };
 })();
 
-const amy = players.createPlayer("Amy", "x");
-console.log(amy);
-
-// const amy = players.createPlayer("Amy", "x");
-// console.log(amy);
+// players.askForPlayerName();
+// const arr = players.getPlayersList();
+// console.log(arr[0].name);
 
 // GAME CONTROLLER obj
 // who starts the game? random seems the best
@@ -70,13 +80,27 @@ console.log(amy);
 // is the selected cell free or already occupied?
 const gameController = (function () {
     function playRound() {
-        const row = prompt ("Select row (0-2):");
+        const row = prompt("Select row (0-2):");
         const column = prompt("Select column (0-2)");
         if (gameboard.chosenCellEmpty(row, column)) {
-            gameboard.markChosenCell(row, column, player);
+            gameboard.markChosenCell(row, column);
         }
     }
 
     return { playRound };
 
 })();
+
+// gameController.playRound();
+// gameboard.printBoard();
+
+// 1. ask for the name
+// 2. create board
+// 3. prompt for coords
+// 4. check if we have a winner
+// 5. check if it is a draw
+// 6. repeat 3 - 5
+// 7. print winner and update the score
+// 8. ask if the user wants to play again or not
+// 9. if not, update the page to show the winner of all games
+//    in big flashy letters!
